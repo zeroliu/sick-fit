e169c4ea7e31549be6434547d9e8b89db4ebe71c
Zero Liu <zerolxy@gmail.com>
2020-05-14 15:30:37 -0700

    Supported updating user permission on the backend.
    - Added `updatePermissions` mutation resolver.

--- sick-fits/backend/src/resolvers/user_resolver.ts
+++ sick-fits/backend/src/resolvers/user_resolver.ts
@@ -1,7 +1,44 @@
-import { Resolver, Query, Ctx } from 'type-graphql';
+import {
+  Resolver,
+  Query,
+  Ctx,
+  Mutation,
+  InputType,
+  Field,
+  Arg,
+  ID,
+} from 'type-graphql';
 import { User, UserPermission } from 'src/entity/user';
 import { Context } from 'src/types';
 import { hasPermission } from 'src/utils';
+
+@InputType()
+class UpdatePermissionsInput {
+  @Field(() => ID)
+  userId!: number;
+
+  @Field(() => [UserPermission])
+  permissions!: UserPermission[];
+}
+
+async function canUpdatePermission(userId?: number): Promise<boolean> {
+  if (!userId) {
+    return false;
+  }
+  const user = await User.findOne({ id: userId });
+  if (!user) {
+    return false;
+  }
+  if (
+    !hasPermission(user, [
+      UserPermission.ADMIN,
+      UserPermission.PERMISSION_UPDATE,
+    ])
+  ) {
+    return false;
+  }
+  return true;
+}
 
 @Resolver()
 export class UserResolver {
@@ -15,21 +52,23 @@ export class UserResolver {
 
   @Query(() => [User])
   async users(@Ctx() ctx: Context): Promise<User[]> {
-    if (!ctx.req.userId) {
-      throw new Error('You must be logged in!');
-    }
-    const user = await User.findOne({ id: ctx.req.userId });
-    if (!user) {
-      throw new Error('User was not found.');
-    }
-    if (
-      !hasPermission(user, [
-        UserPermission.ADMIN,
-        UserPermission.PERMISSION_UPDATE,
-      ])
-    ) {
-      throw new Error('You do not have sufficient permission.');
+    if (!(await canUpdatePermission(ctx.req.userId))) {
+      throw new Error('You do not have sufficient permissions.');
     }
     return await User.find();
+  }
+
+  @Mutation(() => Boolean)
+  async updatePermissions(
+    @Arg('data') data: UpdatePermissionsInput,
+    @Ctx() ctx: Context,
+  ): Promise<boolean> {
+    if (!(await canUpdatePermission(ctx.req.userId))) {
+      throw new Error('You do not have sufficient permissions.');
+    }
+    await User.update(data.userId, {
+      permissions: data.permissions,
+    });
+    return true;
   }
 }
