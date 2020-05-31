a39073ffe62e7ce4d61c454c5feee0e741081f1a
Zero Liu <zerolxy@gmail.com>
2020-05-31 00:23:21 -0700

    Fixed prod backend endpoint

--- sick-fits/frontend/src/lib/withData.tsx
+++ sick-fits/frontend/src/lib/withData.tsx
@@ -1,11 +1,11 @@
 import ApolloClient from 'apollo-boost';
 import withApollo from 'next-with-apollo';
 
-import { endpoint } from 'src/config';
+import { endpoint, prodEndpoint } from 'src/config';
 
 export default withApollo(({ headers }) => {
   return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+    uri: process.env.NODE_ENV === 'development' ? endpoint : prodEndpoint,
     request: (operation) => {
       operation.setContext({
         fetchOptions: {
